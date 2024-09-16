72778fac9d397ac719912a785ad00b965707873b
big00002 <6629011018@cdti.ac.th>
2024-09-16 16:18:07 +0700

    update

--- project1/members/models.py
+++ project1/members/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
 class Member(models.Model):
-  firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255)
-  phone = models.IntegerField()
-  email = models.CharField(max_length=255)
-  joined_date = models.DateField()
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
